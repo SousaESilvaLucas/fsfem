83b208434f0b35323380731c4609cbafa18d42e0
Lucas Sousa e Silva <sousaesilvalucas@gmail.com>
2020-11-06 13:45:04 -0300

    Added Teapot route

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Oi Wanessinha!');
+	res.send('Good Job!');
 });
+
+app.get('/teapot', (req, res) => {
+	res.set({
+		'x-Hello': 'World'
+	});
+	res.status(418);
+	res.send("I'm a teapot!");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
